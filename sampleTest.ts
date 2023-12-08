45989b36f5aa20fa4b9ea37984056df4016472d6
Alex Yemelyanenko <ayemelyanenko@chegg.com>
2023-12-08 15:09:02 -0800

    First commit

---
+++ sampleTest.ts
@@ -0,0 +1,10 @@
+const axios = require('axios');
+
+fixture('Getting Started')
+    .page('https://devexpress.github.io/testcafe/example');
+
+test('My first test', async t => {
+    await t
+        .typeText('#developer-name', 'John Smith')
+        .click('#submit-button');
+});
